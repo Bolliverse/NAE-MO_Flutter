89891db461a10a6b3d6360da2ccae1e6f946d9d1
이동훈 <ldh019@cau.ac.kr>
2026-07-31 18:43:16 +0900

    feat: add task kind to create params

--- lib/features/task/domain/usecases/params/create_task_params.dart
+++ lib/features/task/domain/usecases/params/create_task_params.dart
@@ -1,5 +1,10 @@
+import 'package:nae_mo/features/task/domain/entities/task.dart';
+import 'package:nae_mo/features/task/domain/validation/task_validator.dart';
+
 class CreateTaskParams {
   final String title;
+  final TaskKind kind;
+  final DateTime targetDate;
   final String? categoryId;
   final bool hasTime;
   final DateTime? startDateTime;
@@ -8,10 +13,22 @@ class CreateTaskParams {
 
   const CreateTaskParams({
     required this.title,
+    required this.kind,
+    required this.targetDate,
     this.categoryId,
     this.hasTime = false,
     this.startDateTime,
     this.endDateTime,
     this.isAllDay = false,
   });
+
+  TaskDraft toDraft() => TaskDraft(
+        kind: kind,
+        targetDate: targetDate,
+        isCompleted: false,
+        hasTime: hasTime,
+        startDateTime: startDateTime,
+        endDateTime: endDateTime,
+        isAllDay: isAllDay,
+      );
 }
