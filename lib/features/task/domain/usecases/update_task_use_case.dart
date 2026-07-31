a425d482fd2a6e005c2d2ca90f6d80ab3295276f
이동훈 <ldh019@cau.ac.kr>
2026-07-31 19:04:49 +0900

    feat: validate task updates

--- lib/features/task/domain/usecases/update_task_use_case.dart
+++ lib/features/task/domain/usecases/update_task_use_case.dart
@@ -3,6 +3,7 @@ import 'package:nae_mo/features/task/data/repositories/task_repository_provider.
 import 'package:nae_mo/features/task/domain/entities/task.dart';
 import 'package:nae_mo/features/task/domain/repositories/task_repository.dart';
 import 'package:nae_mo/features/task/domain/usecases/params/update_task_params.dart';
+import 'package:nae_mo/features/task/domain/validation/task_validator.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'update_task_use_case.g.dart';
@@ -11,8 +12,17 @@ class UpdateTaskUseCase {
   final TaskRepository _repository;
   const UpdateTaskUseCase(this._repository);
 
-  Future<Result<Task>> call(UpdateTaskParams params) =>
-      _repository.updateTask(params);
+  Future<Result<Task>> call(UpdateTaskParams params) async {
+    final currentResult = await _repository.getTaskById(params.id);
+    final readFailure = currentResult.failure;
+    if (readFailure != null) return fail<Task>(readFailure);
+
+    final draft = params.resolve(currentResult.data!);
+    final validationFailure = TaskValidator.validate(draft);
+    if (validationFailure != null) return fail<Task>(validationFailure);
+
+    return _repository.updateTask(params);
+  }
 }
 
 @riverpod
