6c945cdbaf030f032cef82278032b5898de9dcc8
이동훈 <ldh019@cau.ac.kr>
2026-08-03 16:42:30 +0900

    feat: add Today presentation state

---
+++ lib/features/calendar/presentation/states/today_state.dart
@@ -0,0 +1,31 @@
+import 'package:nae_mo/features/calendar/domain/entities/today_overview.dart';
+
+class TodayState {
+  final TodayOverview overview;
+  final bool isOverdueExpanded;
+  final bool isCompletedExpanded;
+  final Set<String> pendingTodoIds;
+
+  const TodayState({
+    required this.overview,
+    this.isOverdueExpanded = false,
+    this.isCompletedExpanded = false,
+    this.pendingTodoIds = const {},
+  });
+
+  TodayState copyWith({
+    TodayOverview? overview,
+    bool? isOverdueExpanded,
+    bool? isCompletedExpanded,
+    Set<String>? pendingTodoIds,
+  }) {
+    return TodayState(
+      overview: overview ?? this.overview,
+      isOverdueExpanded: isOverdueExpanded ?? this.isOverdueExpanded,
+      isCompletedExpanded: isCompletedExpanded ?? this.isCompletedExpanded,
+      pendingTodoIds: Set.unmodifiable(
+        pendingTodoIds ?? this.pendingTodoIds,
+      ),
+    );
+  }
+}
